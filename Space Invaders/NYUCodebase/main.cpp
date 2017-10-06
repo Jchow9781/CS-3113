0486f8bf736bc3f19317489b5024871461857b84
Jonchow77 <Jonchow77@gmail.com>
2017-10-05 21:37:20 -0400

    Added spaceship entity

--- Space Invaders/NYUCodebase/main.cpp
+++ Space Invaders/NYUCodebase/main.cpp
@@ -4,6 +4,10 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+#include "ShaderProgram.h"
+#include "Matrix.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #ifdef _WINDOWS
 	#define RESOURCE_FOLDER ""
@@ -13,15 +17,55 @@
 
 SDL_Window* displayWindow;
 
+GLuint LoadTexture(const char *filePath) {
+	int w, h, comp;
+	unsigned char* image = stbi_load(filePath, &w, &h, &comp, STBI_rgb_alpha);
+	if (image == NULL) {
+		std::cout << "Unable to load image. Make sure the path is correct\n";
+		assert(false);
+	}
+	GLuint retTexture;
+	glGenTextures(1, &retTexture);
+	glBindTexture(GL_TEXTURE_2D, retTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	stbi_image_free(image);
+	return retTexture;
+}
+
+struct Spaceship {
+	Spaceship(float left, float right, float top, float bottom) : left(left), right(right), top(top), bottom(bottom) {}
+
+	float left;
+	float right;
+	float top;
+	float bottom;
+};
+
 int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
 	#ifdef _WINDOWS
 		glewInit();
 	#endif
+	
+	//Setup
+	glViewport(0, 0, 1280, 720);
+	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+
+	GLuint whiteTexture = LoadTexture(RESOURCE_FOLDER"white.png");
+
+
+	Matrix projectionMatrix;
+	projectionMatrix.SetOrthoProjection(-3.55f, 3.55f, -2.0f, 2.0f, -1.0f, 1.0f);
+	
+	Matrix spaceship_Matrix;
+
+	Spaceship spaceship(-0.5f, 0.5f, -0.5f, 0.5f);
 
 	SDL_Event event;
 	bool done = false;
@@ -32,6 +76,28 @@ int main(int argc, char *argv[])
 			}
 		}
 		glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(displayWindow);
+
+		glUseProgram(program.programID);
+
+		//Draw spaceship
+		program.SetModelviewMatrix(spaceship_Matrix);
+		program.SetProjectionMatrix(projectionMatrix);
+
+		glBindTexture(GL_TEXTURE_2D, whiteTexture);
+
+		float spaceship_vertices[] = { -0.125f, -1.75f, 0.125f, -1.75f, 0.125f, -1.5f, -0.125f, -1.75f, 0.125f, -1.5f, -0.125f, -1.5f };
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, spaceship_vertices);
+		glEnableVertexAttribArray(program.positionAttribute);
+
+		float texCoords[] = { 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0 };
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDisableVertexAttribArray(program.positionAttribute);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
 		SDL_GL_SwapWindow(displayWindow);
 	}
 
