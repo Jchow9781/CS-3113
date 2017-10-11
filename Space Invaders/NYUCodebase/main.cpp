dd6f2ea467107d2e947b0197f91378dedaa0d061
Jonchow77 <Jonchow77@gmail.com>
2017-10-11 00:30:21 -0400

    Added Entity Class

--- Space Invaders/NYUCodebase/main.cpp
+++ Space Invaders/NYUCodebase/main.cpp
@@ -4,6 +4,7 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+#include <vector>
 #include "ShaderProgram.h"
 #include "Matrix.h"
 #define STB_IMAGE_IMPLEMENTATION
@@ -34,13 +35,87 @@ GLuint LoadTexture(const char *filePath) {
 	return retTexture;
 }
 
-struct Spaceship {
-	Spaceship(float left, float right, float top, float bottom) : left(left), right(right), top(top), bottom(bottom) {}
+//GAME VALUES
+enum TYPE {PLAYER, ALIEN};
+enum STATE {MAIN_MENU, IN_GAME};
+ShaderProgram* program;
 
-	float left;
-	float right;
-	float top;
-	float bottom;
+//Spaceship or alien
+struct Entity {
+	float position[2];
+	float speed[2];
+	float boundary[4];
+	float u;
+	float v;
+	float width;
+	float height;
+	float size = 1.0f;
+	Matrix entityMatrix;
+	TYPE type;
+
+	Entity() {}
+
+	Entity(float x, float y, float spriteU, float spriteV, float spriteWidth, float spriteHeight, float dx, float dy) {
+		position[0] = x;
+		position[1] = y;
+		speed[0] = dx;
+		speed[1] = dy;
+		entityMatrix.Identity();
+		entityMatrix.Translate(x, y, 0);
+		boundary[0] = y + 0.05f * size;
+		boundary[1] = y - 0.05f * size;
+		boundary[2] = x - 0.05f * size;
+		boundary[3] = x + 0.05f * size;
+
+		u = spriteU;
+		v = spriteV;
+		width = spriteWidth;
+		height = spriteHeight;
+	}
+
+	void draw() {
+		entityMatrix.Identity();
+		entityMatrix.Translate(position[0], position[1], 0);
+		program->SetModelviewMatrix(entityMatrix);
+		//Make separate header folder for shader program and pass through pointer
+		
+
+		std::vector<float> vertexData;
+		std::vector<float> texCoordData;
+		float texture_x = u;
+		float texture_y = v;
+		vertexData.insert(vertexData.end(), {
+			(-0.1f * size), 0.1f * size,
+			(-0.1f * size), -0.1f * size,
+			(0.1f * size), 0.1f * size,
+			(0.1f * size), -0.1f * size,
+			(0.1f * size), 0.1f * size,
+			(-0.1f * size), -0.1f * size,
+		});
+		texCoordData.insert(texCoordData.end(), {
+			texture_x, texture_y,
+			texture_x, texture_y + height,
+			texture_x + width, texture_y,
+			texture_x + width, texture_y + height,
+			texture_x + width, texture_y,
+			texture_x, texture_y + height,
+		});
+
+		glUseProgram(program->programID);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertexData.data());
+		glEnableVertexAttribArray(program->positionAttribute);
+		glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoordData.data());
+		glEnableVertexAttribArray(program->texCoordAttribute);
+		glBindTexture(GL_TEXTURE_2D, spriteSheetTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glDisableVertexAttribArray(program->positionAttribute);
+		glDisableVertexAttribArray(program->texCoordAttribute);
+	}
+
 };
 
 int main(int argc, char *argv[])
@@ -57,15 +132,11 @@ int main(int argc, char *argv[])
 	glViewport(0, 0, 1280, 720);
 	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
 
-	GLuint whiteTexture = LoadTexture(RESOURCE_FOLDER"white.png");
+	GLuint mainTexture = LoadTexture(RESOURCE_FOLDER"sheet.png");
 
 
 	Matrix projectionMatrix;
 	projectionMatrix.SetOrthoProjection(-3.55f, 3.55f, -2.0f, 2.0f, -1.0f, 1.0f);
-	
-	Matrix spaceship_Matrix;
-
-	Spaceship spaceship(-0.5f, 0.5f, -0.5f, 0.5f);
 
 	SDL_Event event;
 	bool done = false;
@@ -84,9 +155,11 @@ int main(int argc, char *argv[])
 		program.SetModelviewMatrix(spaceship_Matrix);
 		program.SetProjectionMatrix(projectionMatrix);
 
-		glBindTexture(GL_TEXTURE_2D, whiteTexture);
-
-		float spaceship_vertices[] = { -0.125f, -1.75f, 0.125f, -1.75f, 0.125f, -1.5f, -0.125f, -1.75f, 0.125f, -1.5f, -0.125f, -1.5f };
+		spaceship_Matrix.Identity();
+		//glBindTexture(GL_TEXTURE_2D, whiteTexture);
+		spaceship_Matrix.Translate(0, -1.75f, 0);
+		//float spaceship_vertices[] = { -0.125f, -1.75f, 0.125f, -1.75f, 0.125f, -1.5f, -0.125f, -1.75f, 0.125f, -1.5f, -0.125f, -1.5f };
+		float spaceship_vertices[] = { -0.125f, -0.125f, 0.125f, -0.125f, 0.125f, 0.125f, -0.125f, -0.125f, 0.125f, 0.125f, -0.125f, 0.125f };
 		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, spaceship_vertices);
 		glEnableVertexAttribArray(program.positionAttribute);
 
